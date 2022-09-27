85e33c48d5af8b1f12862a11d0e347d00e64edea
zuberiya <zuberiya139@gmail.com>
2022-09-27 11:29:02 +0530

    navbar

--- components/page/Home/whyUs.tsx
+++ components/page/Home/whyUs.tsx
@@ -1,8 +1,8 @@
 const WhyUs = () => (
     <section>
         <div className="text-center text-[#800000]">
-        <h1 className="font-semibold text-[32px]  py-2">Our Strategy</h1>
-        <h6 className="font-semibold text-[22px]">Process To Find Better Match For You</h6>
+            <h1 className="font-semibold text-[32px]  py-2">Our Strategy</h1>
+            <h6 className="font-semibold text-[22px]">Process To Find Better Match For You</h6>
         </div>
     </section>
 );
--- pages/registerDetails.tsx
+++ pages/registerDetails.tsx
@@ -1,14 +1,65 @@
 const registerDetails = () => (
     <>
-        <p className="mt-20 ">Please provide us with your basic details</p>
-        <input type="text" placeholder="Enter Full Name.." />
-        <input type="email" placeholder="Enter Email.." />
-        <input type="text" placeholder="Enter DOB.." />
-        <input type="text" placeholder="Enter DOB.." />
-        <input type="text" placeholder="Enter Section.." />
-        <input type="text" placeholder="Enter Division.." />
-        <input type="text" placeholder="Enter DOB.." />
-        <input type="password" placeholder="Enter Password.." />
+        <form onSubmit={(e) => e.preventDefault()}>
+            <div className="flex flex-col shadow-lg bg-white w-1/3 px-5">
+                <p className="mt-20 ">Please provide us with your basic details</p>
+                <div className="py-4">
+                    <input
+                        className="border w-full h-10 border-gray-400"
+                        type="text"
+                        placeholder="Enter Full Name.."
+                        required
+                    />
+                </div>
+                <div className="py-4">
+                    <input
+                        className="border w-full h-10 border-gray-400"
+                        type="email"
+                        placeholder="Enter Email.."
+                        required
+                    />
+                </div>
+
+                <div className="py-4">
+                    <input
+                        className="border w-full h-10 border-gray-400"
+                        type="date"
+                        placeholder="Enter DOB.."
+                        required
+                    />
+                </div>
+
+                <div className="py-4">
+                    <input
+                        className="border w-full h-10 border-gray-400"
+                        type="text"
+                        placeholder="Enter Section.."
+                        required
+                    />
+                </div>
+
+                <div className="py-4">
+                    <input
+                        className="border w-full h-10 border-gray-400"
+                        type="text"
+                        placeholder="Enter Division.."
+                        required
+                    />
+                </div>
+
+                <div className="py-4">
+                    <input
+                        className="border w-full h-10 border-gray-400"
+                        type="password"
+                        placeholder="Enter Password.."
+                        required
+                    />
+                </div>
+                <button className="bg-orange-400 h-10" type="submit">
+                    Next
+                </button>
+            </div>
+        </form>
     </>
 );
 
