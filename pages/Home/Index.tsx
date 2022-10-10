5453a5bbaae605446f0e8f51d494f0c6e56ab0a2
Irbazbyte <irbaz2000@gmail.com>
2022-10-10 12:00:17 +0530

    changes

--- pages/Home/Index.tsx
+++ pages/Home/Index.tsx
@@ -1,9 +1,15 @@
 import WhyUs from 'components/Home/WhyUs';
 import WhyChooseUs from 'components/Home/WhyChooseUs';
+import Register from 'components/Home/Register';
+import SectionOne from 'components/Home/SectionOne';
+import SectionTwo from 'components/Home/SectionTwo';
 
 const Home = () => (
     <section>
+        <Register />
+        <SectionOne />
         <WhyChooseUs />
+        <SectionTwo />
         <WhyUs />
     </section>
 );
