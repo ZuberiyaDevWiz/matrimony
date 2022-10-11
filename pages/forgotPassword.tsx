abe76b30b4932fa0039695e016ef44b7926d1d32
Irbazbyte <irbaz2000@gmail.com>
2022-10-11 18:43:45 +0530

    First commit

--- pages/forgotPassword.tsx
+++ pages/forgotPassword.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-unresolved */
-import ForgotPassword from 'components/ForgotPassword';
+import ForgotPassword from 'components/ForgotPassword/Index';
 
 const ForgotPasswordPage = () => (
     <>
