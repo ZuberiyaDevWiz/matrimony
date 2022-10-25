12958adc1ae1dde57e017c0156ab8978fe2423a1
zuberiya <zuberiya139@gmail.com>
2022-10-25 13:34:25 +0530

    changes

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Input from 'components/Form/Input';
 import Button from 'components/Form/Button';
 import Image from 'next/image';
@@ -8,6 +9,7 @@ import Link from 'next/link';
 
 const submitHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    Router.push('/profile');
 };
 
 const otpHandler = () => {
@@ -68,22 +70,33 @@ const Login = () => {
                         </div>
                         <div>
                             <Link href="/forgotPassword">
-                                <p className="pl-32 text-blue-700">Forgot password?</p>
+                                <p className="pl-32 text-blue-700 cursor-pointer">
+                                    Forgot password?
+                                </p>
                             </Link>
                         </div>
                     </div>
                     <div className=" py-7 w-full mx-auto  ">
                         <Button text="Login" color="two" rounded submit />
                     </div>
-                    <div className="flex text-xs space-x-4 items-center w-1/2 mx-auto">
-                        <hr className="w-full h-[2px] bg-black opacity-20" />
-                        <p>OR</p>
-                        <hr className="w-full h-[2px] bg-black opacity-20" />
-                    </div>
-                    <div className="w-full mt-4 mx-auto pb-4">
-                        <Button text="Login with OTP" color="two" rounded onClick={otpHandler} />
-                    </div>
                 </form>
+                <div className="flex text-xs space-x-4 items-center w-1/2 mx-auto">
+                    <hr className="w-full h-[2px] bg-black opacity-20" />
+                    <p>OR</p>
+                    <hr className="w-full h-[2px] bg-black opacity-20" />
+                </div>
+                <div className="w-full mt-4 mx-auto pb-4">
+                    <Button text="Login with OTP" color="two" rounded onClick={otpHandler} />
+                </div>
+
+                <div className="flex justify-center space-x-2">
+                    <p>Don't have an account? </p>
+                    <Link href="register">
+                        <span className="hover:underline cursor-pointer hover:underline-offset-2 tracking-wide">
+                            Create an account
+                        </span>
+                    </Link>
+                </div>
             </div>
         </section>
     );
