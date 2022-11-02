154fa9e6a6ca7cdb59bf83224f5becbfa51106a8
Irbazbyte <irbaz2000@gmail.com>
2022-11-02 13:56:13 +0530

    changes

---
+++ components/Usersetting/UserMembeList.tsx
@@ -0,0 +1,46 @@
+const UserMemberList = [
+    {
+        title: 'Age',
+        value: '18-28',
+        url: 'Edit',
+        id: 1,
+    },
+    {
+        title: 'Religion',
+        value: 'Muslim',
+        url: 'Edit',
+        id: 2,
+    },
+    {
+        title: 'Community',
+        value: 'Open to all',
+        url: 'Edit',
+        id: 3,
+    },
+    {
+        title: 'Mother Tongue',
+        value: 'Open to All',
+        url: 'Edit',
+        id: 4,
+    },
+    {
+        title: 'Height',
+        value: '4.5 - 7',
+        url: 'Edit',
+        id: 5,
+    },
+    {
+        title: 'Marital Status',
+        value: 'Open to All',
+        url: 'Edit',
+        id: 6,
+    },
+    {
+        title: 'Country living in',
+        value: 'Open to All',
+        url: 'Edit',
+        id: 7,
+    },
+];
+
+export default UserMemberList;
